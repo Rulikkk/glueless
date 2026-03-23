adf9585e8c86caa6489d11e7bb85e62a11074fa5
Rulikkk <mustafin.rustem@gmail.com>
2026-03-23 21:48:58 +0300

    More ok!

--- hiddify-core/test.sh
+++ hiddify-core/test.sh
@@ -1,5 +1,5 @@
 docker build . -t my_glueless
-docker run -ti --rm --cap-add NET_ADMIN -v /home/rulikkk/src/glueless/hiddify-core/config/proxy-config.json:/hiddify/proxy-config.json my_glueless sh
+docker run -ti --rm --cap-add NET_ADMIN -v /home/rulikkk/src/glueless/hiddify-core/config:/config my_glueless sh
 docker container prune -f
 docker image rm my_glueless -f
 
